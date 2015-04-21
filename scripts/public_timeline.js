b68c40d1711242ce6f1e4919045dccb80174c69e
Pev <pev.arfan@gmail.com>
2015-04-21 17:27:32 +0200

    Edit Timeline

--- scripts/public_timeline.js
+++ scripts/public_timeline.js
@@ -1,24 +1,25 @@
-$(window).load(function() {
+$('#panel-responsive-page1').on('pagebeforeshow', function(){
 	$.ajax({ 
 		type: "GET", 
 		url: "http://iris.local/minitwit/api/timeline", 
 		dataType:'json',
 		success: function(response){
-			message = "toto"
-
-			$("#timeline").html(message)
 
 			for(key in response.response){
-				$("#timeline").append("<tr>"
-					+ "<td>"+response.response[key]["from"]+"</td>"
-	    			+ "<td>"+response.response[key]["pub_date"]+"</td>"
-	    			+ "<td>"+response.response[key]["text"]+"</td>"
-	    			+ "</tr>")
-			}
+				message = "<li data-icon=\"false\"><a href=\"index.html\">"
+				    +"<h2>"+response.response[key]["from"]+"</h2>"
+				    +"<p>"+response.response[key]["text"]+"</p>"
+				    +"<p class=\"ui-li-aside\"><strong>"+new Date(response.response[key]["pub_date"]*1000)+"</strong></p>"
+				    +"</a></li>"
+				//alert(message)
+				$("#timeline").append(message)
+			}			
+
+			$("#timeline").listview("refresh")
 
 		},  
 		error: function(){
 			alert("fail load json");
 		},
 	});	
-});
+})
