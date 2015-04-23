ab3c5ff5fb7aa508f906fc61452548699ed9a826
Pev <pev.arfan@gmail.com>
2015-04-23 20:07:22 +0200

    Get Element from Form

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,10 @@
+function test () {
+	var nameValue = document.getElementById("pseudo").value;	
+	alert(nameValue)
+}
+
 function submittest(element){
- 
+
         var $this = $(this); // L'objet jQuery du formulaire
  
         // Je récupère les valeurs
